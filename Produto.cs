5f8dd81c84c6b833e5e8193953db8a5d3494c0b1
ryan <ryanferraz.barbosa@gmail.com>
2020-06-30 10:26:58 -0300

    lista feita

--- Produto.cs
+++ Produto.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.IO;
+using System;
 
 namespace Aula27_28_29_30
 {
@@ -24,7 +26,39 @@ namespace Aula27_28_29_30
         public void Cadastrar(Produto prod){
             string[] linha = new string[] { PrepararLinha(prod) };
             File.AppendAllLines(PATH, linha);
+        } 
+
+        public List<Produto> Ler(){
+            // Lista de produto
+            List<Produto> produtos = new List<Produto>();
+
+            // Linhas tranformadas em array de strings
+            string[] linhas = File.ReadAllLines(PATH);
+
+            // Varremos esse array de strings
+            foreach (var linha in linhas){ 
+
+                // Quebra de linha em partes
+                string[] dados = linha.Split(";");
+
+                // Tratamos e adicionamos dados em um novo produto
+                Produto prod = new Produto();
+                prod.Codigo = Int32.Parse( SepararDado(dados[0]));
+                prod.Nome = SepararDado(dados[1]);
+                prod.Preco = float.Parse(SepararDado(dados[2]));
+
+                // Adicionado o produto antes de retorna-lo
+                produtos.Add(prod);
+            }
+            return produtos;
         }
+        public string SepararDado(string dados){
+            return dados.Split("=")[1];
+        }
+        
+        
+
+        
         private string PrepararLinha(Produto p){
             return $"codigo={p.Codigo};nome={p.Nome};preço={p.Preco}";
         }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Aula27_28_29_30
 {
@@ -10,6 +11,13 @@ namespace Aula27_28_29_30
             p1.Codigo = 1;
             p1.Nome = "Coca-cola";
             p1.Preco = 4.50f;
+
+            List<Produto> list = new List<Produto>();
+            list = p1.Ler();
+
+            foreach(Produto item in list){
+                System.Console.WriteLine($"R$:{item.Preco} - {item.Nome}");
+            }
 
             p1.Cadastrar(p1);
         }
